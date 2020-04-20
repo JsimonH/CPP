4f8fbf1db57afe60098dc5f0072f4b45addf8f53
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-04-20 20:26:17 +0800

    Sum_Solution

---
+++ Sum_Solution.c
@@ -0,0 +1,38 @@
+/*
+求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）
+*/
+
+class Sum {
+public:
+    Sum() 
+    {
+        _sum += _i;
+        ++_i;
+    }
+
+    static void Init()
+    {
+        _i = 1;
+        _sum = 0;
+    }
+
+    static int GetSum() 
+    {
+        return _sum;
+    }
+
+private:
+    static int _i;
+    static int _sum;
+};
+int Sum::_i = 1;
+int Sum::_sum = 0;
+
+class Solution {
+public:
+    int Sum_Solution(int n) {
+        Sum::Init();
+        Sum a[n];
+        return Sum::GetSum();
+    }
+};
