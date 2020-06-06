76fb83c105af2795eccb23d4645d39309bd35137
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-06-06 23:14:28 +0800

    Create mergeTwoLists.cpp

---
+++ mergeTwoLists.cpp
@@ -0,0 +1,51 @@
+/* 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+*/
+
+// 虚拟头节点迭代
+class Solution1 {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* head = dummy;
+        while (l1 && l2) 
+        {
+            if (l1->val <= l2->val) 
+            {
+                head->next = l1;
+                l1 = l1->next;
+            } 
+            else 
+            {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+        head->next = l1 == NULL ? l2 : l1;
+        return dummy->next;
+    }
+};
+
+// 递归
+class Solution2 {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) 
+            return l2;
+
+        if (l2 == NULL) 
+            return l1;
+
+        if (l1->val <= l2->val) 
+        {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+};
