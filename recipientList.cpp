85791d7adc546497bacfdf1d866051fc989aba43
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-07-10 12:21:09 +0800

    Create recipientList.cpp

---
+++ recipientList.cpp
@@ -0,0 +1,51 @@
+/* 收件人列表
+NowCoder每天要给许多客户写电子邮件。正如你所知，如果一封邮件中包含多个收件人，收件人姓名之间会用一个逗号和空格隔开；
+如果收件人姓名也包含空格或逗号，则姓名需要用双引号包含。现在给你一组收件人姓名，请你帮他生成相应的收件人列表。
+
+输入描述:
+输入包含多组数据。
+每组数据的第一行是一个整数n (1≤n≤128)，表示后面有n个姓名。
+紧接着n行，每一行包含一个收件人的姓名。姓名长度不超过16个字符。
+
+输出描述:
+对应每一组输入，输出一行收件人列表。
+
+示例：
+输入
+3
+Joe
+Quan, William
+Letendre,Bruce
+2
+Leon
+Kewell
+
+输出
+Joe, “Quan, William”, “Letendre,Bruce”
+Leon, Kewell
+*/
+
+// write your code here cpp
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		getchar();
+		string str;
+		for (int i = 0; i < n; ++i)
+		{
+			getline(cin, str);
+			if (str.find(' ') != -1 || str.find(',') != -1)
+				cout << '"' << str << '"';
+			else
+				cout << str;
+			(i + 1 != n) ? cout << ", " : cout << endl;//最后一个不用输出逗号空格按要求输出换行
+		}
+	}
+	return 0;
+}
