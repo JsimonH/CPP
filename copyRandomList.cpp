a525bce0069fff5179f6b19310bee95209d35a30
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-08-14 22:36:55 +0800

    Create copyRandomList.cpp

---
+++ copyRandomList.cpp
@@ -0,0 +1,76 @@
+/* 复杂链表的复制
+请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+
+示例 1：
+输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+示例 2：
+输入：head = [[1,1],[2,1]]
+输出：[[1,1],[2,1]]
+
+示例 3：
+输入：head = [[3,null],[3,0],[3,null]]
+输出：[[3,null],[3,0],[3,null]]
+
+示例 4：
+输入：head = []
+输出：[]
+解释：给定的链表为空（空指针），因此返回 null。
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+	Node* copyRandomList(Node* head) {
+		if (head == NULL) {
+			return NULL;
+		}
+		
+		//拷贝链表，并且把链表连接起来
+		Node* curr = head;
+		while (curr != NULL) {
+			Node* next = curr->next;
+			Node* copy = new Node(curr->val);
+			curr->next = copy;
+			copy->next = next;
+			curr = next;
+		}
+		
+		//指定随机指针
+		curr = head;
+		while (curr != NULL) {
+			Node* next = curr->next->next; //原节点的下一个节点
+			Node* currCopy = curr->next; //当前的拷贝节点
+			currCopy->random = curr->random != NULL ? curr->random->next : NULL; //指定随机指针节点
+			curr = next; //当前节点指向下一个节点
+		}
+
+		//拆分链表
+		curr = head;
+		Node* res = curr->next; //用于返回的拷贝头节点
+		while (curr != NULL) {
+			Node* next = curr->next->next; //原节点的下一个节点
+			Node* curCopy = curr->next; //拷贝节点
+			curr->next = next; //将原节点指向下一个原节点
+			curCopy->next = next != NULL ? next->next : NULL; //拷贝节点的下一个节点指向下一个原节点的下一个节点
+			curr = next; //将当前节点指向下一个原节点
+		}
+		return res;
+	}
+};
