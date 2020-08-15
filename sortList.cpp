5f896248e9b06e6b9a0b6ee7b16944b75f5b6f89
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-08-16 00:47:48 +0800

    Update sortList.cpp

--- sortList.cpp
+++ sortList.cpp
@@ -1,21 +1,47 @@
-/* 排序链表 */
+/* 排序链表
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+Input: 4->2->1->3
+Output: 1->2->3->4
+
+Example 2:
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode* pre = head, *slow = head, *fast = head;
+        if(head == NULL || head->next == NULL) 
+            return head;
+        
+        ListNode* prev = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
         while(fast && fast->next) {
-            pre = slow;
+            prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-        pre->next = nullptr;
+        prev->next = NULL;
         return mergeTwoList(sortList(head), sortList(slow));
     }
+    
     ListNode* mergeTwoList(ListNode* h1, ListNode* h2) {
-        if(!h1) return h2;
-        if(!h2) return h1;
+        if(h1 == NULL) 
+            return h2;
+        if(h2 == NULL) 
+            return h1;
         if(h1->val < h2->val) {
             h1->next = mergeTwoList(h1->next, h2);
             return h1;
