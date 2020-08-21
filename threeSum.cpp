33076acd0f1150a4bcb3d0c901bb8ba3059719d0
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-08-21 18:23:58 +0800

    Create threeSum.cpp

---
+++ threeSum.cpp
@@ -0,0 +1,52 @@
+/* 三数之和
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+The solution set must not contain duplicate triplets.
+
+Example:
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (right > left) {
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    //去重
+                    while (right > left && nums[right] == nums[right - 1]) 
+                        right--;
+                    while (right > left && nums[left] == nums[left + 1]) 
+                        left++;
+
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+};
