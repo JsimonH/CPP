9e35f71d86fcac89f8cbee2c4d3137e6b6de444f
JsimonH Do <55374999+JsimonH@users.noreply.github.com>
2020-08-26 23:33:20 +0800

    Create search.cpp

---
+++ search.cpp
@@ -0,0 +1,50 @@
+/* 搜索旋转排序数组
+Given an integer array nums sorted in ascending order, and an integer target.
+Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+You should search for target in nums and if you found return its index, otherwise return -1.
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+Input: nums = [1], target = 0
+Output: -1
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.empty())
+            return -1;
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //前半部分有序
+            if (nums[start] <= nums[mid]) {
+                //target在前半部分
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
